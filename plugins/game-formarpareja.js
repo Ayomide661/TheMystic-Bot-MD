d27707735fd7ac5f434bad518f347ac889a2c0f3
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-04 21:48:42 +0100

    Update game-formarpareja.js

--- plugins/game-formarpareja.js
+++ plugins/game-formarpareja.js
@@ -1,21 +1,16 @@
 const toM = (a) => '@' + a.split('@')[0];
 function handler(m, {groupMetadata}) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.fun_formarpareja
-
   const ps = groupMetadata.participants.map((v) => v.id);
   const a = ps.getRandom();
   let b;
   do b = ps.getRandom();
   while (b === a);
-  m.reply(`*${toM(a)}, ${tradutor.texto1[0]} ${toM(b)}, ${tradutor.texto1[1]}`, null, {
+  m.reply(`*${toM(a)}, you should date ${toM(b)}, you two look perfect together!*`, null, {
     mentions: [a, b],
   });
 }
-handler.help = ['formarpareja'];
+handler.help = ['matchmake'];
 handler.tags = ['game'];
-handler.command = ['formarpareja', 'formarparejas'];
+handler.command = ['matchmake', 'matchmaker', 'pairup', 'ship'];
 handler.group = true;
 export default handler;
