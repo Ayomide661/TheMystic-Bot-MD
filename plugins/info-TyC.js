ce8bf05171ac0796d0a189f4f4a1fe8c115fb989
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-04 13:31:33 +0100

    Update info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -1,18 +1,15 @@
-
 const handler = async (m, {conn}) => {
-  const datas = global
-   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-   const tradutor = _translate.plugins.info_tyc
-   
-   global.terminos = tradutor.texto1;
+  const datas = global;
+  const language = datas.db.data.users[m.sender].language || global.defaultLanguage;
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${language}.json`));
+  const translator = _translate.plugins.info_tos; // Changed from info_tyc to info_tos (Terms of Service)
+  
+  global.terms = translator.texto1; // Changed variable names to English
 
-  m.reply(global.terminos);
+  m.reply(global.terms);
 };
 
-handler.help = ['tyc'];
+handler.help = ['tos']; // Changed from 'tyc' to 'tos' (Terms of Service)
 handler.tags = ['info'];
-handler.command = /^(términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones)$/i;
+handler.command = /^(termsandconditions|termsandprivacy|termsofservice|tos|terms of service|terms and conditions|privacy policy|terms of use|conditions of use)$/i;
 export default handler;
-
-
