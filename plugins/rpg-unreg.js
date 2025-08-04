a0904e22e61fc5f94b0950266991d398e032976c
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-04 21:57:03 +0100

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,20 +1,18 @@
 import {createHash} from 'crypto';
 
-
 const handler = async function(m, {args}) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.rpg_unreg
-
-  if (!args[0]) throw tradutor.texto1;
+  if (!args[0]) throw "Please enter your serial number to unregister";
+  
   const user = global.db.data.users[m.sender];
   const sn = createHash('md5').update(m.sender).digest('hex');
-  if (args[0] !== sn) throw tradutor.texto2;
+  
+  if (args[0] !== sn) throw "Invalid serial number!";
+  
   user.registered = false;
-  m.reply(tradutor.texto3);
+  m.reply("Successfully unregistered! Your account has been removed from the database.");
 };
-handler.help = ['unreg <numero de serie>'];
+
+handler.help = ['unreg <serial number>'];
 handler.tags = ['xp'];
 handler.command = /^unreg(ister)?$/i;
 handler.register = true;
