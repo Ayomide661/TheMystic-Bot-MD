72beb783e98e4533f254b856da83b6138981799b
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-05 18:59:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import fs from 'fs'; 
 import moment from 'moment-timezone';
 
-global.botnumber = "2348108629978";
+global.botnumber = "";
 global.confirmCode = "";
 global.authFile = `MysticSession`;
 
