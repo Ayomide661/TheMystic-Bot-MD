6f89fc0de8f5cb542f2d6ed95e36e5c29d5f7a99
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-06 18:19:21 +0100

    Update owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,20 +1,18 @@
-
-
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_reporte
+  const language = datas.db.data.users[m.sender].language || global.defaultLanguage
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${language}.json`))
+  const translator = _translate.plugins.owner_report
 
-  if (!text) throw `${tradutor.texto1[0]}\n*${usedPrefix + command} ${tradutor.texto1[1]} ${usedPrefix}play ${tradutor.texto1[2]}`;
-  if (text.length < 10) throw tradutor.texto2;
-  if (text.length > 1000) throw tradutor.texto3;
-  const teks = `${tradutor.texto4[0]} wa.me/${m.sender.split`@`[0]}\n${tradutor.texto4[1]} ${text}\n*┴*`;
-  conn.reply('5219992095479@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('584125778026@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  m.reply(tradutor.texto5);
+  if (!text) throw `${translator.text1[0]}\n*${usedPrefix + command} ${translator.text1[1]} ${usedPrefix}play ${translator.text1[2]}`;
+  if (text.length < 10) throw translator.text2;
+  if (text.length > 1000) throw translator.text3;
+  const message = `${translator.text4[0]} wa.me/${m.sender.split`@`[0]}\n${translator.text4[1]} ${text}\n*┴*`;
+  conn.reply('5219992095479@s.whatsapp.net', m.quoted ? message + m.quoted.text : message, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('584125778026@s.whatsapp.net', m.quoted ? message + m.quoted.text : message, null, {contextInfo: {mentionedJid: [m.sender]}});
+  m.reply(translator.text5);
 };
-handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
+handler.help = ['report <text>'];
 handler.tags = ['info'];
-handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes)$/i;
+handler.command = /^report$/i;
 export default handler;
