d38c55b1851deaf1e7c1d6a789ba1eddc66e344a
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-08 05:58:38 +0100

    Update config-funciones.js

--- plugins/config-funciones.js
+++ plugins/config-funciones.js
@@ -465,7 +465,7 @@ break;
       if (!/[01]/.test(command)) return await conn.sendMessage(m.chat, {text: optionsFull}, {quoted: m});
       throw false;
   }
-  conn.sendMessage(m.chat, {text: `_*${tradutor.texto27[0]}*_\n\n*${tradutor.texto27[1]}* _${type}_ *fue* ${isEnable ? '_activada_' : '_desactivada_'} *${tradutor.texto27[2]}* ${isAll ? '_bot._' : isUser ? '' : '_chat._'}`}, {quoted: m});
+  conn.sendMessage(m.chat, {text: `_*${tradutor.texto27[0]}*_\n\n*${tradutor.texto27[1]}* _${type}_ *was* ${isEnable ? '_enabled_' : '_disabled_'} *${tradutor.texto27[2]}* ${isAll ? '_for the bot._' : isUser ? '' : '_for this chat._'}`}, {quoted: m});
 };
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?[01])$/i;
 export default handler;
