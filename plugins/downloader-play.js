3be071511ae363a15828b97154321f86a486e085
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-09 07:34:27 +0100

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -6,7 +6,7 @@ const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
 const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
 const tradutor = _translate.plugins.descargas_play
 
-if (!text) throw `${tradutor.texto1[0]} ${usedPrefix + command} ${tradutor.texto1[1]}`;      
+if (!text) throw `${tradutor.text1[0]} ${usedPrefix + command} ${tradutor.text1[1]}`;      
 let additionalText = '';
 if (['play'].includes(command)) {
  additionalText = 'audio';
@@ -16,7 +16,7 @@ if (['play'].includes(command)) {
 
 const regex = "https://youtube.com/watch?v="
 const result = await search(args.join(' '));
-const body = `${tradutor.texto2[0]} ${result.title}\n${tradutor.texto2[1]} ${result.ago}\n${tradutor.texto2[2]} ${result.duration.timestamp}\n${tradutor.texto2[3]} ${formatNumber(result.views)}\n${tradutor.texto2[4]} ${result.author.name}\n${tradutor.texto2[5]} ${result.videoId}\n${tradutor.texto2[6]} ${result.type}\n${tradutor.texto2[7]} ${result.url}\n${tradutor.texto2[8]} ${result.author.url}\n\n${tradutor.texto2[9]} ${additionalText}, ${tradutor.texto2[10]}`.trim();
+const body = `${tradutor.text2[0]} ${result.title}\n${tradutor.text2[1]} ${result.ago}\n${tradutor.text2[2]} ${result.duration.timestamp}\n${tradutor.text2[3]} ${formatNumber(result.views)}\n${tradutor.text2[4]} ${result.author.name}\n${tradutor.text2[5]} ${result.videoId}\n${tradutor.text2[6]} ${result.type}\n${tradutor.text2[7]} ${result.url}\n${tradutor.text2[8]} ${result.author.url}\n\n${tradutor.text2[9]} ${additionalText}, ${tradutor.text2[10]}`.trim();
 conn.sendMessage(m.chat, { image: { url: result.thumbnail }, caption: body }, { quoted: m });
 
 if (command === 'play') {
@@ -26,7 +26,7 @@ const downloader = audiodlp.download;
 conn.sendMessage(m.chat, { audio: { url: downloader }, mimetype: "audio/mpeg" }, { quoted: m });
 } catch (error) {
  console.log(error);
- conn.reply(m.chat, tradutor.texto6, m);
+ conn.reply(m.chat, tradutor.text6, m);
  }
 }
 
@@ -37,7 +37,7 @@ const downloader = videodlp.download;
 conn.sendMessage(m.chat, { video: { url: downloader }, mimetype: "video/mp4" }, { quoted: m });
 } catch (error) {
  console.log(error);
- conn.reply(m.chat, tradutor.texto6, m);
+ conn.reply(m.chat, tradutor.text6, m);
   }
  }
 };
