3488d76ba236c3934fa0defdf7edeed78cdeedf6
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-09 02:23:22 +0100

    Update sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -15,12 +15,12 @@ let text
         text = args.slice(0).join(" ");
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text;
-    } else throw tradutor.texto1;
-   if (!text) return m.reply(tradutor.texto2);
+    } else throw tradutor.text1;
+   if (!text) return m.reply(tradutor.text2);
     const who = await m.mentionedJid && await await m.mentionedJid[0] ? await await m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
     const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
     const mishi = text.replace(mentionRegex, '');
-   if (mishi.length > 30) return m.reply(tradutor.texto3);
+   if (mishi.length > 30) return m.reply(tradutor.text3);
     const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
     const nombre = await conn.getName(who)
     const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
