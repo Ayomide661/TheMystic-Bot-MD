d5967fab684e1a235fc90d5e4ccc785b509fb2b9
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-09 19:54:01 +0100

    Update search-yts.js

--- plugins/search-yts.js
+++ plugins/search-yts.js
@@ -2,11 +2,11 @@ import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from "
 import yts from 'yt-search';
 import fs from 'fs';
 
-const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
+const handler = async (m, { conn, text, usedPrefix: prefix }) => {
     const datas = global;
-    const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
-    const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
-    const traductor = _translate.plugins.buscador_yts;
+    const language = datas.db.data.users[m.sender].language || global.defaultLanguage;
+    const _translate = JSON.parse(fs.readFileSync(`./src/languages/${language}.json`));
+    const traductor = _translate.plugins.search_yts;
     const device = await getDevice(m.key.id);
 
   if (!text) throw `⚠️ *${traductor.text1}*`;
@@ -14,14 +14,14 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
   if (device !== 'desktop' || device !== 'web') {      
 
   const results = await yts(text);
-  if (!results || !results?.videos) return m.reply('> *[❗] Error: Videos not founds.*')    
+  if (!results || !results?.videos) return m.reply('> *[❗] Error: Videos not found.*')    
   const videos = results.videos.slice(0, 20);
   const randomIndex = Math.floor(Math.random() * videos.length);
   const randomVideo = videos[randomIndex];
 
   var messa = await prepareWAMessageMedia({ image: {url: randomVideo.thumbnail}}, { upload: conn.waUploadToServer })
   const interactiveMessage = {
-    body: { text: `*—◉ Resultados obtenidos:* ${results.videos.length}\n*—◉ Video aleatorio:*\n*-› Title:* ${randomVideo.title}\n*-› Author:* ${randomVideo.author.name}\n*-› Views:* ${randomVideo.views}\n*-› ${traductor.text2[0]}:* ${randomVideo.url}\n*-› Imagen:* ${randomVideo.thumbnail}`.trim() },
+    body: { text: `*—◉ Search results:* ${results.videos.length}\n*—◉ Random video:*\n*-› Title:* ${randomVideo.title}\n*-› Author:* ${randomVideo.author.name}\n*-› Views:* ${randomVideo.views}\n*-› ${traductor.text2[0]}:* ${randomVideo.url}\n*-› Thumbnail:* ${randomVideo.thumbnail}`.trim() },
     footer: { text: `${global.wm}`.trim() },  
       header: {
           title: `*< YouTube Search />*\n`,
@@ -33,21 +33,21 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
         {
           name: 'single_select',
           buttonParamsJson: JSON.stringify({
-            title: 'OPCIONES DISPONIBLES',
+            title: 'AVAILABLE OPTIONS',
             sections: videos.map((video) => ({
               title: video.title,
               rows: [
                 {
                   header: video.title,
                   title: video.author.name,
-                  description: 'Descargar MP3',
-                  id: `${prefijo}ytmp3 ${video.url}`
+                  description: 'Download MP3',
+                  id: `${prefix}ytmp3 ${video.url}`
                 },
                 {
                   header: video.title,
                   title: video.author.name,
-                  description: 'Descargar MP4',
-                  id: `${prefijo}ytmp4 ${video.url}`
+                  description: 'Download MP4',
+                  id: `${prefix}ytmp4 ${video.url}`
                 }
               ]
             }))
@@ -69,9 +69,9 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
 
   } else {
   const datas = global;
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
-  const traductor = _translate.plugins.buscador_yts;      
+  const language = datas.db.data.users[m.sender].language || global.defaultLanguage;
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${language}.json`));
+  const traductor = _translate.plugins.search_yts;      
   const results = await yts(text);
   const tes = results.all;
   const teks = results.all.map((v) => {
@@ -89,5 +89,5 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
 };
 handler.help = ['ytsearch <text>'];
 handler.tags = ['search'];
-handler.command = /^(ytsearch|yts|searchyt|buscaryt|videosearch|audiosearch)$/i;
+handler.command = /^(ytsearch|yts|searchyt|ytsearch|videosearch|audiosearch)$/i;
 export default handler;
