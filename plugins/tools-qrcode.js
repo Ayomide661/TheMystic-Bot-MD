897387ebb9308237f3275605fbc642e3e77b6d3f
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-09 02:08:46 +0100

    Update tools-qrcode.js

--- plugins/tools-qrcode.js
+++ plugins/tools-qrcode.js
@@ -6,7 +6,7 @@ const handler = async (m, {text, conn}) => {
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.plugins.herramientas_qrcode
 
-  if (!text) throw tradutor.texto1;
+  if (!text) throw tradutor.text1;
   conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), {scale: 8}), 'qrcode.png', '¯\\_(ツ)_/¯', m);
 };
 handler.help = ['', 'code'].map((v) => 'qr' + v + ' <teks>');
