70d43b54d648ce1bcaab5b151626d435ee27f0a9
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-09 02:07:44 +0100

    Update tools-readqrcode.js

--- plugins/tools-readqrcode.js
+++ plugins/tools-readqrcode.js
@@ -9,12 +9,12 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw tradutor.texto1;
+  if (!mime) throw tradutor.text1;
   const img = await q.download?.();
   const url = await uploadImage(img);
   const anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`);
   const json = await anu.json();
-  await m.reply(`${tradutor.texto2} ${json.result}`);
+  await m.reply(`${tradutor.text2} ${json.result}`);
 };
 handler.command = /^(readqr)$/i;
 export default handler;
