e8931d4cdec13e311d61c536d29194254691409e
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-09 13:13:37 +0100

    Update owner-addmsg.js.js

--- plugins/owner-addmsg.js.js
+++ plugins/owner-addmsg.js.js
@@ -1,5 +1,3 @@
-
-
 const handler = async (m, {command, usedPrefix, text}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
@@ -8,12 +6,12 @@ const handler = async (m, {command, usedPrefix, text}) => {
 
   const M = m.constructor;
   const which = command.replace(/agregar/i, '');
-  if (!m.quoted) throw tradutor.texto1;
-  if (!text) throw `${tradutor.texto2[0]} *${usedPrefix}list${which}* ${tradutor.texto2[1]}`;
+  if (!m.quoted) throw tradutor.text1;
+  if (!text) throw `${tradutor.text2[0]} *${usedPrefix}list${which}* ${tradutor.text2[1]}`;
   const msgs = global.db.data.msgs;
-  if (text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' ${tradutor.texto3}`;
+  if (text in msgs) throw `*[❗𝐈𝐍𝐅𝐎❗] '${text}' ${tradutor.text3}`;
   msgs[text] = M.toObject(await m.getQuotedObj());
-  m.reply(`${tradutor.texto4[0]} '${text}'${tradutor.texto4[1]} ${usedPrefix}ver${which} ${text}*`);
+  m.reply(`${tradutor.text4[0]} '${text}'${tradutor.text4[1]} ${usedPrefix}ver${which} ${text}*`);
 };
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map((v) => 'add' + v + ' <text>');
 handler.tags = ['database'];
