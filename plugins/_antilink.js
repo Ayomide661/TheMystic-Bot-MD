3d23854b862319098e4730adfed28aed761fd799
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-11 14:56:10 +0100

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,6 +1,3 @@
-// TheMystic-Bot-MD@BrunoSobrino - _antilink.js
-
-  
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 export async function before(m, {conn, isAdmin, isBotAdmin}) {
 
@@ -17,19 +14,19 @@ export async function before(m, {conn, isAdmin, isBotAdmin}) {
   const user = `@${m.sender.split`@`[0]}`;
   const isGroupLink = linkRegex.exec(m.text);
   const grupo = `https://chat.whatsapp.com`;
-  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(tradutor.texto1.replace('@user', '@' + user.split('@')[0]));
+  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(tradutor.text1.replace('@user', '@' + user.split('@')[0]));
   if (chat.antiLink && isGroupLink && !isAdmin) {
     if (isBotAdmin) {
       const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
       if (m.text.includes(linkThisGroup)) return !0;
     }
-    await this.sendMessage(m.chat, {text: tradutor.texto2.replace('@user', '@' + user.split('@')[0]), mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply(tradutor.texto3);
+    await this.sendMessage(m.chat, {text: tradutor.text2.replace('@user', '@' + user.split('@')[0]), mentions: [m.sender]}, {quoted: m});
+    if (!isBotAdmin) return m.reply(tradutor.text3);
     if (isBotAdmin && bot.restrict) {
       await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
-    } else if (!bot.restrict) return m.reply(tradutor.texto4);
+    } else if (!bot.restrict) return m.reply(tradutor.text4);
   }
   return !0;
 }
