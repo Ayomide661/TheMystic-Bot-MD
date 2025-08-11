ce70a53dadcb6b5181008861698c1953f92e1710
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-11 14:47:06 +0100

    Update group-setwelcome.js

--- plugins/group-setwelcome.js
+++ plugins/group-setwelcome.js
@@ -6,8 +6,8 @@ const handler = async (m, {conn, text, isROwner, isOwner}) => {
 
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text;
-    m.reply(tradutor.texto1);
-  } else throw `${tradutor.texto2[0]}\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
+    m.reply(tradutor.text1);
+  } else throw `${tradutor.text2[0]}\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
 };
 handler.help = ['setwelcome <text>'];
 handler.tags = ['group'];
