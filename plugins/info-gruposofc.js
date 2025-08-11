d08a2791f2b99f4cdee0f25fd422cf7ea92f1d94
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-11 16:31:27 +0100

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -6,9 +6,9 @@ const handler = async (m, {conn, usedPrefix}) => {
 
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `${tradutor.texto1[0]}
+  const text = `${tradutor.text1[0]}
 
-${tradutor.texto1[1]}
+${tradutor.text1[1]}
 1.- https://chat.whatsapp.com/LjJbmdO0qSDEKgB60qivZj
 
 2.- https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
@@ -23,19 +23,19 @@ ${tradutor.texto1[1]}
 
 2.- https://chat.whatsapp.com/Fy74b6fgE9SJJpHVi6CKJY`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`},
+    'document': {url: `https://github.com/Ayomide661/TheMystic-Bot-MD`},
     'mimetype': `application/${document}`,
-    'fileName': `${tradutor.texto2}`,
+    'fileName': `${tradutor.text2}`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaUrl': 'https://github.com/Ayomide661/TheMystic-Bot-MD',
         'mediaType': 2,
         'previewType': 'pdf',
-        'title': `${tradutor.texto3}`,
+        'title': `${tradutor.text3}`,
         'body': wm,
         'thumbnail': imagen1,
         'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
