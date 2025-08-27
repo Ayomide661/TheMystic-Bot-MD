e84cd602964047af7cd782ace7261b01edca1721
The goated 🗿 <106935269+Ayomide661@users.noreply.github.com>
2025-08-27 08:02:12 +0100

    Update game-ttt.js

--- plugins/game-ttt.js
+++ plugins/game-ttt.js
@@ -7,14 +7,14 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
   const tradutor = _translate.plugins.game_ttt
 
   conn.game = conn.game ? conn.game : {};
-  if (Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw tradutor.texto6;
-  if (!text) throw `${tradutor.texto1[0]}\n*◉ ${usedPrefix + command} ${tradutor.texto1[1]}`;
+  if (Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw tradutor.text6;
+  if (!text) throw `${tradutor.text1[0]}\n*◉ ${usedPrefix + command} ${tradutor.text1[1]}`;
   let room = Object.values(conn.game).find((room) => room.state === 'WAITING' && (text ? room.name === text : true));
   if (room) {
-    await m.reply(tradutor.texto2);
+    await m.reply(tradutor.text2);
     room.o = m.chat;
     room.game.playerO = m.sender;
-    room.state = tradutor.texto3;
+    room.state = tradutor.text3;
     const arr = room.game.render().map((v) => {
       return {
         X: '❎',
@@ -40,7 +40,7 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
         ${arr.slice(3, 6).join('')}
         ${arr.slice(6).join('')}
 
-${tradutor.texto4} @${room.game.currentTurn.split('@')[0]}
+${tradutor.text4} @${room.game.currentTurn.split('@')[0]}
 `.trim();
     if (room.x !== room.o) await conn.sendMessage(room.x, {text: str, mentions: this.parseMention(str)}, {quoted: m});
     await conn.sendMessage(room.o, {text: str, mentions: conn.parseMention(str)}, {quoted: m});
@@ -53,10 +53,12 @@ ${tradutor.texto4} @${room.game.currentTurn.split('@')[0]}
       state: 'WAITING'};
     if (text) room.name = text;
     const imgplay = `https://cope-cdnmed.agilecontent.com/resources/jpg/8/9/1590140413198.jpg`;
-    conn.reply(m.chat, `${tradutor.texto5[0]} ${usedPrefix}delttt*\n\n${tradutor.texto5[1]} (${usedPrefix + command} ${text})`, m);
+    conn.reply(m.chat, `${tradutor.text5[0]} ${usedPrefix}delttt*\n\n${tradutor.text5[1]} (${usedPrefix + command} ${text})`, m);
     // conn.sendButton(m.chat, `*🕹 𝐓𝐑𝐄𝐒 𝐄𝐍 𝐑𝐀𝐘𝐀 🎮*\n\n*◉ 𝙴𝚂𝙿𝙴𝚁𝙰𝙽𝙳𝙾 𝙰𝙻 𝚂𝙴𝙶𝚄𝙽𝙳𝙾 𝙹𝚄𝙶𝙰𝙳𝙾𝚁*\n*◉ 𝙿𝙰𝚁𝙰 𝙱𝙾𝚁𝚁𝙰𝚁 𝙾 𝚂𝙰𝙻𝙸𝚁𝚂𝙴 𝙳𝙴 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝚄𝚂𝙴𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}delttt*`, wm, imgplay, [['𝚄𝙽𝙸𝚁𝚂𝙴 𝙰 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
     conn.game[room.id] = room;
   }
 };
-handler.command = /^(tictactoe|ttc|ttt|xo)$/i;
+
+// Cambiado para que solo responda al comando 'ttc'
+handler.command = /^(ttc)$/i;
 export default handler;
